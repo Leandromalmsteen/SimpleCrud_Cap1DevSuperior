8a74fed0f28b1848f815857ffda5c5ae8da6b855
Leandro Nunes <llopesnunes@gmail.com>
2021-09-26 08:49:19 -0300

    Seeding, DTO

--- backend/src/main/java/com/devsuperior/simple_crud/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/simple_crud/services/ClientService.java
@@ -1,10 +1,13 @@
 package com.devsuperior.simple_crud.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.simple_crud.dto.ClientDTO;
 import com.devsuperior.simple_crud.entities.Client;
 import com.devsuperior.simple_crud.repositories.ClientRepository;
 
@@ -14,7 +17,9 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 
-	public List<Client> findAll(){
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll(){
+		List<Client> list =  repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 }
