b38a63ec55732951f8e9d4aba41194740f90a694
Leandro Nunes <llopesnunes@gmail.com>
2021-09-26 11:37:55 -0300

    Exceptions, update client, delete client

--- backend/src/main/java/com/devsuperior/simple_crud/services/exceptions/DatabaseException.java
+++ backend/src/main/java/com/devsuperior/simple_crud/services/exceptions/DatabaseException.java
@@ -1,9 +1,9 @@
 package com.devsuperior.simple_crud.services.exceptions;
 
-public class EntityNotFoundException extends RuntimeException {
+public class DatabaseException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
-	public EntityNotFoundException(String msg) {
+	public DatabaseException(String msg) {
 		super(msg);
 	}
 
